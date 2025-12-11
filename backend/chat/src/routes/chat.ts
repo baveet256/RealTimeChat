4529912f99d312fe52ac87bd9057d9a398c54655
Baveet Hora <bhora@asu.edu>
2025-12-10 19:27:13 -0700

    implement chat messaging functionality with Cloudinary integration and multer for Image uploads

--- backend/chat/src/routes/chat.ts
+++ backend/chat/src/routes/chat.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import dotenv from 'dotenv';
 import isAuth from '../middlewares/isAuth.js';
-import { createNewChat } from "../controllers/chat.js";
+import { createNewChat, getAllChats, getMessagesOfChat, sendMessage } from "../controllers/chat.js";
 
+import { upload } from '../middlewares/multer.js';
 dotenv.config()
 
 const router = express.Router();
 
 router.post("/chat/new",isAuth, createNewChat);
+router.get("/chat/all",isAuth, getAllChats);
+router.post("/message",isAuth,upload.single("file"), sendMessage);
+router.get("/message/:chatId",isAuth, getMessagesOfChat);
 
 export default router;
